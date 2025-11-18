20f66ae7fc9689c1554b7d013ddfced16cbf3dbe
Spoony <ginolol612@gmail.com>
2025-11-18 17:13:11 -0300

    variables de entorno

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
+import os
 from app import create_app
 from dotenv import load_dotenv
 
 load_dotenv()
+DEBUG= os.getenv('FLASK_DEBUG', True)
+HOST = os.getenv('SERVER_HOST', '0.0.0.0')
+PORT = os.getenv('SERVER_PORT', 5000)
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=DEBUG, host=HOST, port=PORT)
